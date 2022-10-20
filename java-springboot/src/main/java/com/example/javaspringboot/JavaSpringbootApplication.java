bc3d08b181f620f0280a01036bd272516e9223e2
zhanghuocheng <zhanghuocheng@okay.cn>
2022-10-20 09:05:03 +0800

    up

--- java-springboot/src/main/java/com/example/javaspringboot/JavaSpringbootApplication.java
+++ java-springboot/src/main/java/com/example/javaspringboot/JavaSpringbootApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JavaSpringbootApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(JavaSpringbootApplication.class, args);
     }
-
 }
---
+++ java-springboot/src/main/java/com/example/javaspringboot/aop/LogAop.java
@@ -0,0 +1,8 @@
+package com.example.javaspringboot.aop;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class LogAop {
+
+}
