874a4e954e70506992e78e70e1709d953b2eb2b1
zhanghuocheng <zhanghuocheng@okay.cn>
2022-10-31 14:36:24 +0800

    up

--- java-springboot/src/main/java/com/example/javaspringboot/pdf/PdfboxUtil.java
+++ java-springboot/src/main/java/com/example/javaspringboot/pdf/PdfboxUtil.java
@@ -12,13 +12,12 @@ public class PdfboxUtil {
     public static String PDFPATH = "E:\\非技术书\\《自控力》（凯利·麦格尼格尔-著）.pdf";
     private static String FILEPATH = "D:/Maven权威指南中文版.doc";
     public static void main(String[] args) throws Exception {
-////        PDDocument document = PDDocument.(new File("test.pdf"));
-//        if (!document.isEncrypted()) {
-//            PDFTextStripper stripper = new PDFTextStripper();
-//            String text = stripper.getText(document);
-//            System.out.println("Text:" + text);
-//        }
-//        document.close();
+        PDDocument document = PDDocument.load(new File(PDFPATH));
+        if (!document.isEncrypted()) {
+            PDFTextStripper stripper = new PDFTextStripper();
+            String text = stripper.getText(document);
+            System.out.println("Text:" + text);
+        }
+        document.close();
     }
-
 }
